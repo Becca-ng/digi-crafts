a6fd1701ff59fa42b07c9f064f9bb17d9c18398f
bec <ryyng95@gmail.com>
2022-02-03 21:11:24 -0500

    commit

--- react1/greeter-app/src/app.jsx
+++ react1/greeter-app/src/app.jsx
@@ -2,15 +2,21 @@
 import Header from './sectioning/head.jsx';
 import Main from './sectioning/main.jsx';
 import Footer from './sectioning/foot.jsx';
+import Contact from './components/contacts.jsx';
 import './app.css';
+import { faker } from '@faker-js/faker';
 
 function App() {
   return (
+    <body>
     <div className="y-wrap" >
      <header class = 'head'> <Header /> </header>
+     <Contact />
+     <p>{faker.name.findname()}</p>
      <main class = 'main'> <Main /> </main> 
       <footer id = 'foot' class = 'foot'> <Footer /> </footer>
     </div>
+    </body>
   );
 }
 
---
+++ react1/greeter-app/src/components/contacts.jsx
@@ -0,0 +1,12 @@
+const Contact = () => {
+    return(
+        <li>
+        <h2> Name </h2>
+        <p> Street Address</p>
+        <p>State</p>
+        <p>Email</p>
+        </li>
+    )
+}
+
+export default Contact;
