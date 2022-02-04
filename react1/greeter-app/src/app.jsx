9e6f8787aaa34ae6b3221ecb907fc11e6adb75fc
bec <ryyng95@gmail.com>
2022-02-03 20:38:50 -0500

    css

--- react1/greeter-app/src/app.jsx
+++ react1/greeter-app/src/app.jsx
@@ -2,13 +2,14 @@
 import Header from './sectioning/head.jsx';
 import Main from './sectioning/main.jsx';
 import Footer from './sectioning/foot.jsx';
+import './app.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+    <div className="y-wrap" >
+     <header class = 'head'> <Header /> </header>
+     <main class = 'main'> <Main /> </main> 
+      <footer id = 'foot' class = 'foot'> <Footer /> </footer>
     </div>
   );
 }
